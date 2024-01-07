dec9d2ad8e92b0dede5c50aa164d63b6c5369466
GalRaz <galraz@gmail.com>
2024-01-07 16:29:13 -0500

    graded dishab trial info

--- MIT_tools/MIT_job_array.sh
+++ MIT_tools/MIT_job_array.sh
@@ -2,7 +2,7 @@
 
 project_path="/om2/scratch/tmp/galraz/RANCH/RANCH_cluster"
 
-param_info=$project_path/sim_info/param_info/eig.csv
+param_info=$project_path/sim_info/param_info/KL.csv
 param_dir=$project_path/MIT_tools/param_dir
 
 # code that takes eig.csv and creates different files for each row
