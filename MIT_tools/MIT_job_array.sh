b463619f85022b6a0da73f92d21fd28622087735
GalRaz <galraz@gmail.com>
2024-02-19 21:03:59 -0500

    reorganization

--- MIT_tools/MIT_job_array.sh
+++ MIT_tools/MIT_job_array.sh
@@ -2,11 +2,11 @@
 
 project_path="/om2/scratch/tmp/galraz/RANCH/RANCH_cluster"
 
-param_info=$project_path/sim_info/param_info/infants/eig.csv
-trial_info="RANCH_cluster/sim_info/trial_info/infants/trial_info_exposure_duration.csv"
-embedding_info="RANCH_cluster/sim_info/embeddings/infants/resnet_pa_unscaled.csv"
+param_info=$project_path/sim_info/param_info/adults/eig.csv
+trial_info="RANCH_cluster/sim_info/trial_info/stimulus_type/adults/trial_info.csv"
+embedding_info="RANCH_cluster/sim_info/embeddings/stimulus_type/adults/resnet_pa_unscaled.csv"
 
-param_dir=$project_path/MIT_tools/param_dir
+param_dir=$project_path/MIT_tools/test_param_dir
 
 # code that takes eig.csv and creates different files for each row
 cmd="python $project_path/MIT_tools/populate_param_dir.py $param_info"
